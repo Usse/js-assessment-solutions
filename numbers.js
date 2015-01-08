c7b27526aabc0646e7a11668776cb46d87e98087
Usse <darkusse@gmail.com>
2015-01-08 14:15:13 +0000

    cleanup

--- numbers.js
+++ numbers.js
@@ -12,32 +12,24 @@ define(function() {
 
     convertToBinary: function(num) {
       var arr = [];
-
       for (var i = 7; i > -1; i--) {
         arr.push( num & (1 << i) ? 1 : 0 );
       }
-
       return arr.join('');
     },
 
     multiply: function(a, b) {
       a = adjust(a);
       b = adjust(b);
-
       var result = (a.adjusted * b.adjusted) / (a.multiplier * b.multiplier);
-
       return result;
-
       function adjust(num) {
         var exponent, multiplier;
-
         if (num < 1) {
           exponent = Math.floor( Math.log(num) * -1 );
           multiplier = Math.pow(10, exponent);
-
           return { adjusted: num * multiplier, multiplier: multiplier };
         }
-
         return { adjusted: num, multiplier: 1 };
       }
     }
